6061053f6edcda09565d25a903186604d7f7e55a
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-02-17 09:54:16 +0600

    expense balance section done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,51 @@
+// function getInputTotal(inputId){
+//     const foodInput = document.getElementById(inputId);
+//     const foodInputField = foodInput.value;
+//     const foodInputText = parseInt(foodInputField);
+//     return foodInputText;
+
+// }
+// function getTotalField(elementInnerText){
+//     const totalExpense = document.getElementById(elementInnerText);
+//     const totalExpenseBalance = totalExpense.innerText;
+//     const proviousTotalExp = parseInt(totalExpenseBalance);
+//     // totalExpense.innerText =  getInputTotal;
+//     return proviousTotalExp;
+
+    
+// }
+
+// handle total expense even done
 document.getElementById('calculate-btn').addEventListener('click', function(){
     // get expense input element
     const foodInput = document.getElementById('food-input');
     const foodInputField = foodInput.value;
     const foodInputText = parseInt(foodInputField);
+    // const foodInput = getInputTotal('food-input');
 
    
     const rentInput = document.getElementById('rent-input');
     const rentInputField = rentInput.value;
     const rentInputText = parseInt(rentInputField);
-
+    // const rentInput = getInputTotal('rent-input');
     
     const clothInput = document.getElementById('cloth-input');
     const clothInputField = clothInput.value;
     const clothInputText = parseInt(clothInputField);
-
+    // const clothInput = getInputTotal('cloth-input');
     // total expense calculete
     const totalExpense = document.getElementById('total-exp');
     const totalExpenseBalance = totalExpense.innerText;
-    const totalInputText = parseInt(totalExpenseBalance);
+    const proviousTotalExp = parseInt(totalExpenseBalance);
+    // const totalExp = getTotalField('total-exp');
 
+    // Total expense balance
+    const totalBalanceExpense = foodInputText + rentInputText + clothInputText;
+    // const totalBalanceExpense = foodInput + rentInput + clothInput;
+    totalExpense.innerText =  totalBalanceExpense;
     
+   
+       
 
-
-    
     
 })
